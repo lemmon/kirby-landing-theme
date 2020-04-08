8b6d7c5743495148c8faa74326220d2cdbbc81ff
Lemmon <lemmon@users.noreply.github.com>
2020-04-08 18:16:17 +0200

    text block, copy css

---
+++ site/plugins/kirbytext/index.php
@@ -0,0 +1,51 @@
+<?php
+
+Kirby::plugin('lemmon/kirbytext', [
+  'fieldMethods' => [
+    'kt' => function (Kirby\Cms\Field $field, $args) {
+      return kirbytext($field->value, array_merge([
+        'parent' => $field->parent(),
+        'field'  => $field,
+      ], $args));
+    },
+  ],
+  'tags' => [
+    'image' => [
+      'attr' => [
+        'alt',
+        'title',
+        'caption',
+      ],
+      'html' => function ($tag) {
+        // find file
+        $file = $tag->file($tag->value);
+        if (!$file) return;
+        // params
+        $alt     = $tag->alt     ?? $file->alt()->value();
+        $title   = $tag->title   ?? $file->title()->value();
+        $caption = $tag->caption ?? $file->caption()->value();
+        // responsivity
+        if (!in_array($file->mime(), ['image/gif', 'image/svg+xml'])) {
+          if (!empty($tag->data['srcset'])) {
+            $srcset = $file->srcset($tag->data['srcset']);
+          }
+          if (!empty($tag->data['width'])) {
+            $file = $file->resize($tag->data['width']);
+          }
+        }
+        // html
+        return Html::figure([
+          Html::img($file->url(), [
+            'width'  => $file->width(),
+            'height' => $file->height(),
+            'alt'    => $alt ?? ' ',
+            'title'  => $title,
+            'srcset' => $srcset ?? null,
+          ]),
+        ], $caption ? [
+          kirbytext($caption),
+        ] : null);
+      },
+    ],
+  ],
+]);
